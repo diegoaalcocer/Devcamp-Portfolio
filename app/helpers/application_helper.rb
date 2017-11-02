83594f64beffa5a94326aff9e8f51d125aa4ad73
Diego Alcocer <diegoaalcocer@gmail.com>
2017-11-02 19:48:37 +0000

    Implemented twitter call on tech new page and updated application helper for error

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -17,7 +17,7 @@ module ApplicationHelper
   end
 
   def copyright_generator
-    DevcampViewTool::Renderer.copyright 'Jordan Hudgens', 'All rights reserved'
+    AlcoViewTool::Renderer.copyright 'Diego Alcocer', 'All rights reserved'
   end
 
   def nav_items
